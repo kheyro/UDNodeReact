2ce69e0aa38c23bc384c2bf55335e35415687d43
DESKTOP-53SUGPO\Deniss <denis@displayworldcompany.com>
2017-11-11 14:49:47 +0800

    session 5 - 48

---
+++ config/dev.js
@@ -0,0 +1,7 @@
+// dev.js don't commit
+module.exports = {
+    googleClientID: '587817809422-sm5j8tv4de6c72iu0h34vj7uijv75nr8.apps.googleusercontent.com',
+    googleClientSecret: '_bhe3AfXuepuiMbLUd8iRQMF',
+    mongoURI: 'mongodb://kheyro:password@ds139585.mlab.com:39585/emaily-dev-khey',
+    cookieKey: 'dlsakikellknjPOKak'
+};
