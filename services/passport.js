2467f98331c4fc025c31de0336fc97d129033c19
Denis Atkesone <datkesone@Deniss-MacBook-Pro.local>
2017-11-19 17:54:27 +0800

    Section 7, Lecture 80

--- services/passport.js
+++ services/passport.js
@@ -24,20 +24,17 @@ passport.use(
             callbackURL: '/auth/google/callback',
             proxy: true
         },
-        (accessToken, refreshToken, profile, done) => {
+        async (accessToken, refreshToken, profile, done) => {
             /*console.log('access token:', accessToken);
             console.log('refresh token:', refreshToken);
             console.log('profile:', profile);*/
-            User.findOne({ googleId: profile.id })
-                .then((existingUser) => {
-                    if (existingUser) {
-                        done(null, existingUser);
-                    } else {
-                        new User({ googleId: profile.id })
-                            .save()
-                            .then(user => done(null, user));
-                    }
-                });
+            const existingUser = await User.findOne({ googleId: profile.id });
+            if (existingUser) {
+                return done(null, existingUser);
+            }
+            // we don't have a user with this ID, make a new user
+            const user = await new User({googleId: profile.id}).save();
+            done(null, user);
         }
     )
 );
